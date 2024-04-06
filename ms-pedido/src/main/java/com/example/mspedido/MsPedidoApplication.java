3aa344665543958709e3aa60c723535496479f96
Alessandro Pastor <alexkip159alex@gmail.com>
2024-04-06 08:41:18 -0500

    pedidosss

--- ms-pedido/src/main/java/com/example/mspedido/MsPedidoApplication.java
+++ ms-pedido/src/main/java/com/example/mspedido/MsPedidoApplication.java
@@ -1,7 +1,11 @@
 package com.example.mspedido;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsPedidoApplication {
@@ -9,5 +13,14 @@ public class MsPedidoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsPedidoApplication.class, args);
 	}
-
+	@Bean
+	public OpenAPI custumOpenApi() {
+		return new OpenAPI().info(new Info()
+				.title("OPEN API MICROSERVICIO CLIENTE")
+				.version("0.0.1")
+				.description("servicio web cliente")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
+		);
+	}
 }
