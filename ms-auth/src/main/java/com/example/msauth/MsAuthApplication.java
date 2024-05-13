e69c4502c4f5bd425d2fc62264a82655af78c8bb
Alessandro Pastor <alexkip159alex@gmail.com>
2024-05-13 09:50:12 -0500

    seguridad

---
+++ ms-auth/src/main/java/com/example/msauth/MsAuthApplication.java
@@ -0,0 +1,15 @@
+package com.example.msauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MsAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthApplication.class, args);
+	}
+
+}
---
+++ ms-auth/src/main/java/com/example/msauth/entity/AuthUser.java
@@ -0,0 +1,30 @@
+package com.example.msauth.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class AuthUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userName;
+    private String password;
+
+
+}
