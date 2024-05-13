34a57d9736fe5534e9291b10160ce671b62bfd71
Alessandro Pastor <alexkip159alex@gmail.com>
2024-05-13 10:19:33 -0500

    seguriddaddt

--- ms-auth/src/main/java/com/example/msauth/Repository/AuthRepository.java
+++ ms-auth/src/main/java/com/example/msauth/Repository/AuthRepository.java
@@ -8,7 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface AuthRepository extends JpaRepository<AuthUser, Integer> {
-
-    Optional<AuthUser> findByUserName
-            (String userName);
+    Optional<AuthUser> findByUserName(String userName);
 }
