98b20fdcd0388e104ce3287128cef248e5497ce4
Alessandro Pastor <alexkip159alex@gmail.com>
2024-05-13 09:56:22 -0500

    seguridadata

--- ms-auth/src/main/java/com/example/msauth/entity/AuthUser.java
+++ ms-auth/src/main/java/com/example/msauth/entity/AuthUser.java
@@ -5,8 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- ms-auth/src/main/java/com/example/msauth/entity/TokenDto.java
+++ ms-auth/src/main/java/com/example/msauth/entity/TokenDto.java
@@ -1,11 +1,9 @@
 package com.example.msauth.entity;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @NoArgsConstructor
 @AllArgsConstructor
