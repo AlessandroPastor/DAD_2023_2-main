593258421603ab2fb38de952803d823eb2aba5f7
Alessandro Pastor <alexkip159alex@gmail.com>
2024-05-20 09:19:38 -0500

    matartoken

---
+++ ms-gateway-server/src/main/java/com/example/msgatewayserver/dto/TokenDto.java
@@ -0,0 +1,13 @@
+package com.example.msgatewayserver.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TokenDto {
+    private String token;
+}
